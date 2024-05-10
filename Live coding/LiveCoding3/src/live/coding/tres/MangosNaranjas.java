c29236c32aec1bada4a41d8160f3922edf159ac1
marksalazarx <marksalazarx@gmail.com>
2024-05-10 16:54:46 -0600

    Actualización de código

--- Live coding/LiveCoding3/src/live/coding/tres/MangosNaranjas.java
+++ Live coding/LiveCoding3/src/live/coding/tres/MangosNaranjas.java
@@ -1,42 +1,46 @@
 package live.coding.tres;
 
+import java.util.Scanner;
 
 public class MangosNaranjas {
-	int mangos;
-	int naranjas;
-	
-	public MangosNaranjas(int mangos, int naranjas) {
-		this.mangos = mangos;
-		this.naranjas = naranjas;
-	}
 
-	//Metodo mangosCaja:
-	
-	public void mangosCaja() {
-		int maximo = 0;
-		if(this.mangos % 2 == 0) {
-			for(int i = 1; i <= (this.mangos/2); i ++) {
-				if((this.mangos/2)%i == 0) {
-					maximo = i;
-				}
-			}
-			System.out.println("El mcd: " + maximo);
-		}else {
-			for(int i = 1; i <= ((this.mangos-1)/2); i ++) {
-				if(((this.mangos-1)/2)%i == 0) {
-					maximo = i;
-				}
-			}
-			System.out.println("El mcd es: " + maximo);
-		}
-	}
-	
-	@Override
-	public String toString() {
-		return "MangosNaranjas [mangos=" + this.mangos + ", naranjas=" + this.naranjas + "]";
-	}
-	
-	
-	
+        int mangos;
+        int naranjas;
 
+        void imprimir() {
+            int mangosPorCaja = naranjas / obtenerMaximoDivisor(mangos, naranjas);
+            int naranjasPorCaja = mangos / obtenerMaximoDivisor(naranjas, mangos);
+            int cajas = (mangos + naranjas) / (naranjasPorCaja + mangosPorCaja);
+            System.out.println("Numero de cajas: " + cajas);
+            System.out.println("Numero de mangos por caja: " + mangosPorCaja);
+            System.out.println("Numero de naranjas por caja: " + naranjasPorCaja);
+        }
+
+        int obtenerMaximoDivisor(int a, int b) {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public MangosNaranjas(int mangos, int naranjas) {
+            this.mangos = mangos;
+            this.naranjas = naranjas;
+        }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Numero de mangos: ");
+        int mangos = scanner.nextInt();
+        System.out.print("Numero de naranjas: ");
+        int naranjas = scanner.nextInt();
+        scanner.close();
+
+        MangosNaranjas mangosNaranjas = new MangosNaranjas(mangos, naranjas);
+
+        mangosNaranjas.imprimir();
+
+    }
 }
